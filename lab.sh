f727b2e63fb12cb9edede66fec417bf684a5c28e
Delan Azabani <delan@azabani.com>
2013-08-07 10:33:01 +0800

    refactored lab.sh splitting into remote/lab.sh

--- lab.sh
+++ lab.sh
@@ -2,6 +2,6 @@
 lab=$1
 last=$2
 for i in `seq $last`; do
-	host=`printf '%02d' $i`
-	ssh -o StrictHostKeyChecking=no lab$lab-$host ./mac.sh
+	host="lab$lab-`printf '%02d' $i`"
+	./remote.sh "$host"
 done

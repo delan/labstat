26befb8a80c7e5c255bde19e3034ef822f3a8e8e
Delan Azabani <delan@azabani.com>
2013-08-07 19:11:33 +0800

    fixed cleanup.sh to work with new output format

--- cleanup.sh
+++ cleanup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 file="$1"
 cat "$file" |
-	sort -k 1,1 -k 2r,2 |	# ORDER BY hostname ASC, datetime DESC
-	sort -u -k 1,1		# collapse consecutive same hostname into first
+	sort -k 2,2 -k 4r,4 |	# ORDER BY hostname ASC, datetime DESC
+	sort -u -k 2,2		# collapse consecutive same hostname into first
